ca12d7327ec5e3c63172c045b2ed2a63d525dc4a
J. Renée Beach <splendidnoise@gmail.com>
2012-08-20 16:18:46 +0200

    Improved the navbar height and width calculations.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- js/navbar.js
+++ js/navbar.js
@@ -7,8 +7,16 @@ Drupal.navbar = Drupal.navbar || {};
  */
 Drupal.behaviors.navbar = {
   attach: function(context) {
-    $('body').css('paddingTop', Drupal.navbar.height() + Drupal.navbar.drawerHeight());
-    $('#navbar-drawer').css('top', Drupal.navbar.height());
+    var $bar = $('#navbar');
+    var $drawer = $('#navbar-drawer');
+    var barHeight = $bar.outerHeight()
+    var drawerHeight = $drawer.outerHeight();
+    $('body').css({
+      'padding-top': barHeight + drawerHeight
+    });
+    $drawer.css({
+      'top': barHeight
+    });
     // The following code is just for demo. Please rip it out fast.
     $('#admin-toolbar ul.drilldown-active-menu li').once('navbar-menu-items', function (index, element) {
       $(this).prepend(
@@ -19,28 +27,4 @@ Drupal.behaviors.navbar = {
     });
   }
 };
-
-Drupal.navbar.height = function() {
-  return Drupal.navbar.heightOf('#navbar');
-}
-
-Drupal.navbar.drawerHeight = function() {
-  return Drupal.navbar.heightOf('#navbar-drawer');
-};
-
-Drupal.navbar.heightOf = function(selector) {
-  var $navbar = $(selector);
-  var height = $navbar.outerHeight();
-  // In modern browsers (including IE9), when box-shadow is defined, use the
-  // normal height.
-  var cssBoxShadowValue = $navbar.css('box-shadow');
-  var boxShadow = (typeof cssBoxShadowValue !== 'undefined' && cssBoxShadowValue !== 'none');
-  // In IE8 and below, we use the shadow filter to apply box-shadow styles to
-  // the toolbar. It adds some extra height that we need to remove.
-  if (!boxShadow && /DXImageTransform\.Microsoft\.Shadow/.test($navbar.css('filter'))) {
-    height -= $navbar[0].filters.item("DXImageTransform.Microsoft.Shadow").strength;
-  }
-  return height;
-};
-
 })(jQuery);
