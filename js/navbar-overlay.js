ee1852e88180d160b39654311db342c4b8d4aaaf
J. Renée Beach <splendidnoise@gmail.com>
2013-04-03 15:53:40 -0400

    Drupal.displace is working with Overlay on the JavaScript side. Now for CSS.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- js/navbar-overlay.js
+++ js/navbar-overlay.js
@@ -34,6 +34,58 @@ $.extend(Drupal.overlay, {
   eventhandlerViewportOffsetChange: function (event, offsets) {
     // Allow other scripts to respond to this event.
     $(document).trigger('drupalOverlayResize');
+  },
+
+  /**
+   * Event handler: resizes displaced elements so they won't overlap the scrollbar
+   * of overlay's iframe.
+   *
+   * @param event
+   *   Event being triggered, with the following restrictions:
+   *   - event.type: any
+   *   - event.currentTarget: any
+   */
+  eventhandlerAlterDisplacedElements: function (event) {
+    // Proceed only if the overlay still exists.
+    if (!(this.isOpen || this.isOpening) || this.isClosing || !this.iframeWindow) {
+      return;
+    }
+
+    var offsets = displace.offsets;
+
+    // Move the body of the iframe contentDocument inward a sufficient distance
+    // to prevent it from appearing underneath displacing elements like the
+    // toolbar.
+    var iframeBody = this.iframeWindow.document.body;
+    $(iframeBody).css({
+      'padding-top': offsets.top,
+      'padding-right': offsets.right,
+      'padding-bottom': offsets.bottom,
+      'padding-left': offsets.left
+    });
+    // Trigger a repaint.
+    iframeBody.style.display = 'none';
+    var _tmp = iframeBody.offsetHeight;
+    iframeBody.style.display = 'block';
+
+    // Constrain the width of offsetting top and bottom elements, such as the
+    // toolbar, so that a scroll in the overlay iframe won't be occluded.
+    var iframeBodyWidth = iframeBody.clientWidth;
+    if (iframeBodyWidth > 0 && iframeBodyWidth < document.documentElement.clientWidth) {
+      $('[data-offset-top], [data-offset-bottom]').css('max-width', iframeBodyWidth);
+    }
+  },
+  /**
+   * Event handler: restores size of displaced elements as they were before
+   * overlay was opened.
+   *
+   * @param event
+   *   Event being triggered, with the following restrictions:
+   *   - event.type: any
+   *   - event.currentTarget: any
+   */
+  eventhandlerRestoreDisplacedElements: function (event) {
+    $('[data-offset-top], [data-offset-bottom]').css('max-width', 'none');
   }
 });
 
