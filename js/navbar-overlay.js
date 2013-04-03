3448b5101337c75d146ee0ec2e96b5d14857dc36
J. Renée Beach <splendidnoise@gmail.com>
2013-04-03 15:48:15 -0400

    Added the drupalViewportOffsetChange to the Drupal.overlay object.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

---
+++ js/navbar-overlay.js
@@ -0,0 +1,40 @@
+/**
+ * @file
+ * Overrides methods of the Drupal.overlay object in order to support D8-style
+ * viewport displacement
+ */
+
+(function ($, Drupal, displace) {
+
+"use strict";
+
+/**
+ * Implements Drupal.behaviors.
+ */
+ Drupal.behaviors.NavbarOverlayOverrides = {
+  attach: function (context, settings) {
+    // Attach the Drupal
+    $(document)
+      .bind('drupalViewportOffsetChange.navbar', $.proxy(Drupal.overlay, 'eventhandlerViewportOffsetChange'))
+  }
+ };
+
+$.extend(Drupal.overlay, {
+  /**
+   * Responds to the drupalViewportOffsetChange event.
+   *
+   * @param object event
+   *   A jQuery event object.
+   *
+   * @param object offsets
+   *   An object whose keys are the for sides an element -- top, right, bottom
+   *   and left. The value of each key is the viewport displacement distance for
+   *   that edge.
+   */
+  eventhandlerViewportOffsetChange: function (event, offsets) {
+    // Allow other scripts to respond to this event.
+    $(document).trigger('drupalOverlayResize');
+  }
+});
+
+}(jQuery, Drupal, Drupal.displace));
